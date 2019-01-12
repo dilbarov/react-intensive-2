4ebb0dfc19455cd568e78cc552b910264646ac7d
Evgeny <jackdilbarov@gmail.com>
2019-01-13 01:27:34 +0500

    Удалил Link, с key ошибки не падают

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react';
-import Link from './Link';
+import styled from "styled-components";
+
+const StyledLink = styled.a`
+    color: ${props => props.active ? 'pink' : '#fff'};
+    font-size: 14px;
+    font-weight: 300;
+    line-height: 24px;
+    padding-right: 33px;
+    border-right: 1px dotted #fff;
+    &:hover {
+        color: #8d8d8d;
+        text-decoration: none;
+    }
+    &last-child {
+        border-right: none;
+    }
+`;
+
 
 class Links extends Component {
     render() {
         const List = this.props.links.map(obj =>
-            <Link link={obj.link} text={obj.name} id={obj.id}/>
-        );
+            <StyledLink href={obj.link} key={obj.id} > {obj.name} </StyledLink>
+        )
         return (
             List
         )
